052d4f5c5e92f259fb46a536bafd9eacd1e184e7
Dylan-Renna <121834115+Dylan-Renna@users.noreply.github.com>
2023-01-02 18:21:43 -0500

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,21 @@
+
+<?php
+//get data from form  
+
+/*$name = $_POST['name'];
+$email= $_POST['email'];
+$phone= $_POST['phone'];
+$message= $_POST['order'];
+
+$to = "dylangamez82@mail.com";
+
+$subject = "Customer Order";
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone Number=" . $phone . "\r\n  Order = " . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("Location:thankyou.html");*/
+?>
